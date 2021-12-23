2e8d3db77413ee4319ef7cff06e7c83f8253146b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-12-23 08:00:00 +0100

    Implement IChildIndexProvider

--- VirtualScrollDemo/Controls/VirtualPanel.cs
+++ VirtualScrollDemo/Controls/VirtualPanel.cs
@@ -15,115 +15,7 @@ namespace VirtualScrollDemo.Controls
 {
     public class VirtualPanel : Panel, ILogicalScrollable, IChildIndexProvider
     {
-        public static readonly StyledProperty<IList?> ItemsProperty = 
-            AvaloniaProperty.Register<VirtualPanel, IList?>(nameof(Items));
-
-        public static readonly StyledProperty<double> ItemHeightProperty = 
-            AvaloniaProperty.Register<VirtualPanel, double>(nameof(ItemHeight), double.NaN);
-
-        public static readonly StyledProperty<IDataTemplate?> ItemTemplateProperty = 
-            AvaloniaProperty.Register<VirtualPanel, IDataTemplate?>(nameof(ItemTemplate));
-
-        public IList? Items
-        {
-            get => GetValue(ItemsProperty);
-            set => SetValue(ItemsProperty, value);
-        }
-
-        public double ItemHeight
-        {
-            get => GetValue(ItemHeightProperty);
-            set => SetValue(ItemHeightProperty, value);
-        }
-
-        [Content]
-        public IDataTemplate? ItemTemplate
-        {
-            get => GetValue(ItemTemplateProperty);
-            set => SetValue(ItemTemplateProperty, value);
-        }
-        
-
-
-
-        private int _startIndex = -1;
-        private int _endIndex = -1;
-        private List<IControl> _controls = new List<IControl>();
-
-        private Size CalculateSize(Size size)
-        {
-            _viewport = size;
-
-            var itemCount = Items.Count;
-            var itemHeight = ItemHeight;
-            var height = itemCount * itemHeight;
-
-            size = size.WithHeight(height);
-
-            _extent = size;
-
-            
-            _scrollSize = new Size(16, 16);
-            _pageScrollSize = new Size(_viewport.Width, _viewport.Height);
-            
-            
-            return size;
-        }
-
-        private void Materialize(Size viewport, Size extent, Vector offset, out double topOffset, [CallerMemberName] string name = default)
-        {
-            var itemCount = Items.Count;
-            var itemHeight = ItemHeight;
-
-            var startIndex = (int)(offset.Y / itemHeight);
-            var visibleCount = (int)(viewport.Height / itemHeight);
-            var endIndex = startIndex + visibleCount - 1;
-
-            topOffset = offset.Y % itemHeight;
-
-            Debug.WriteLine($"viewport: {viewport}" +
-                            $", extent: {extent}" +
-                            $", offset: {offset}" +
-                            $", startIndex: {startIndex}" +
-                            $", endIndex: {endIndex}" +
-                            $", visibleCount: {visibleCount}" +
-                            $", topOffset: {-topOffset}" +
-                            $", name: {name}");
-
-            if (_controls.Count == 0)
-            {
-                var index = startIndex;
-                for (var i = 0; i < visibleCount; i++)
-                {
-                    var param = Items[index];
-                    var control = ItemTemplate.Build(param);
-                    control.DataContext = param;
-                    _controls.Add(control);
-                    index++;
-                }
-
-                foreach (var control in _controls)
-                {
-                    Children.Add(control);
-                }
-            }
-            else
-            {
-                var index = startIndex;
-                for (var i = 0; i < visibleCount; i++)
-                {
-                    var param = Items[index];
-                    var control = _controls[i];
-                    control.DataContext = param;
-                    index++;
-                }  
-            }
-
-
-        }
-        
-        
-        
+        #region ILogicalScrollable
 
         private Size _extent = new Size();
         private Vector _offset = new Vector();
@@ -193,6 +85,162 @@ namespace VirtualScrollDemo.Controls
             remove => _scrollInvalidated -= value;
         }
 
+        private void InvalidateScrollable()
+        {
+            if (this is not ILogicalScrollable scrollable)
+            {
+                return;
+            }
+
+            scrollable.RaiseScrollInvalidated(EventArgs.Empty);
+        }
+
+        #endregion
+
+        #region IChildIndexProvider
+
+        private EventHandler<ChildIndexChangedEventArgs> _childIndexChanged;
+
+        int IChildIndexProvider.GetChildIndex(ILogical child)
+        {
+            if (child is IControl control)
+            {
+                var indexOf = _controls.IndexOf(control);
+                var index = _indexes[indexOf];
+                // Debug.WriteLine($"{indexOf} -> {index}");
+                return index;
+            }
+
+            return -1;
+        }
+
+        bool IChildIndexProvider.TryGetTotalCount(out int count)
+        {
+            count = Items.Count;
+            return true;
+        }
+
+        event EventHandler<ChildIndexChangedEventArgs> IChildIndexProvider.ChildIndexChanged
+        {
+            add => _childIndexChanged += value;
+            remove => _childIndexChanged -= value;
+        }
+
+        #endregion
+
+        #region Properties
+                
+        public static readonly StyledProperty<IList?> ItemsProperty = 
+            AvaloniaProperty.Register<VirtualPanel, IList?>(nameof(Items));
+
+        public static readonly StyledProperty<double> ItemHeightProperty = 
+            AvaloniaProperty.Register<VirtualPanel, double>(nameof(ItemHeight), double.NaN);
+
+        public static readonly StyledProperty<IDataTemplate?> ItemTemplateProperty = 
+            AvaloniaProperty.Register<VirtualPanel, IDataTemplate?>(nameof(ItemTemplate));
+
+        public IList? Items
+        {
+            get => GetValue(ItemsProperty);
+            set => SetValue(ItemsProperty, value);
+        }
+
+        public double ItemHeight
+        {
+            get => GetValue(ItemHeightProperty);
+            set => SetValue(ItemHeightProperty, value);
+        }
+
+        [Content]
+        public IDataTemplate? ItemTemplate
+        {
+            get => GetValue(ItemTemplateProperty);
+            set => SetValue(ItemTemplateProperty, value);
+        }
+
+        #endregion
+
+        #region Layout
+
+        private int _startIndex = -1;
+        private int _endIndex = -1;
+        private List<IControl> _controls = new List<IControl>();
+        private List<int> _indexes = new List<int>();
+
+        private Size CalculateSize(Size size)
+        {
+            _viewport = size;
+
+            var itemCount = Items.Count;
+            var itemHeight = ItemHeight;
+            var height = itemCount * itemHeight;
+
+            size = size.WithHeight(height);
+
+            _extent = size;
+
+            
+            _scrollSize = new Size(16, 16);
+            _pageScrollSize = new Size(_viewport.Width, _viewport.Height);
+            
+            
+            return size;
+        }
+
+        private void Materialize(Size viewport, Size extent, Vector offset, out double topOffset, [CallerMemberName] string name = default)
+        {
+            var itemCount = Items.Count;
+            var itemHeight = ItemHeight;
+
+            var startIndex = (int)(offset.Y / itemHeight);
+            var visibleCount = (int)(viewport.Height / itemHeight);
+            var endIndex = startIndex + visibleCount - 1;
+
+            topOffset = offset.Y % itemHeight;
+/*
+            Debug.WriteLine($"viewport: {viewport}" +
+                            $", extent: {extent}" +
+                            $", offset: {offset}" +
+                            $", startIndex: {startIndex}" +
+                            $", endIndex: {endIndex}" +
+                            $", visibleCount: {visibleCount}" +
+                            $", topOffset: {-topOffset}" +
+                            $", name: {name}");
+*/
+            if (_controls.Count == 0)
+            {
+                var index = startIndex;
+                for (var i = 0; i < visibleCount; i++)
+                {
+                    var param = Items[index];
+                    var control = ItemTemplate.Build(param);
+                    control.DataContext = param;
+                    _controls.Add(control);
+                    _indexes.Add(index);
+                    index++;
+                }
+
+                foreach (var control in _controls)
+                {
+                    Children.Add(control);
+                }
+            }
+            else
+            {
+                var index = startIndex;
+                for (var i = 0; i < visibleCount; i++)
+                {
+                    var param = Items[index];
+                    var control = _controls[i];
+                    control.DataContext = param;
+                    _indexes[i] = index;
+                    index++;
+                }  
+            }
+
+            _childIndexChanged?.Invoke(this, new ChildIndexChangedEventArgs());
+        }
+
         protected override Size MeasureOverride(Size availableSize)
         {
             availableSize = CalculateSize(availableSize);
@@ -205,7 +253,7 @@ namespace VirtualScrollDemo.Controls
                 {
                     var size = new Size(_viewport.Width, ItemHeight);
                     control.Measure(size);
-                    Debug.WriteLine($"Measure: {size}");
+                    //Debug.WriteLine($"Measure: {size}");
                 }
             }
 
@@ -229,7 +277,7 @@ namespace VirtualScrollDemo.Controls
                     var rect = new Rect(new Point(0, y), new Size(_viewport.Width, ItemHeight));
                     control.Arrange(rect);
                     y += ItemHeight;
-                    Debug.WriteLine($"Arrange: {rect}");
+                    //Debug.WriteLine($"Arrange: {rect}");
                 }
             }
 
@@ -237,14 +285,6 @@ namespace VirtualScrollDemo.Controls
             return finalSize;
         }
 
-        private void InvalidateScrollable()
-        {
-            if (this is not ILogicalScrollable scrollable)
-            {
-                return;
-            }
-
-            scrollable.RaiseScrollInvalidated(EventArgs.Empty);
-        }
+        #endregion
     }
 }
